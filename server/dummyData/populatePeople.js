6cb3c2fa97d66a0030903b5a5c78c1663d245fb8
gillysuit <contemporarygil@gmail.com>
2019-12-12 17:40:25 -0800

    Add button, async fetch logic, and full name rendering

--- server/dummyData/populatePeople.js
+++ server/dummyData/populatePeople.js
@@ -2,10 +2,24 @@ const Person = require("../models/personModel");
 
 module.exports = () => {
   // brought you by the most popular gender neutral names for 2020 random article
-  const firstNames = ["Charlie", "Finley", "Skyler", "Justice", "Royal"];
+  const firstNames = [
+    "Charlie",
+    "Finley",
+    "Skyler",
+    "Justice",
+    "Royal",
+    "Jane"
+  ];
   // this is just a continuation of the list
-  const middleNames = ["Oakley", "Lennon", "Armani", "Azariah", "Robin"];
-  const lastNames = ["Cypress", "Indiana", "Ridley", "Storm", "Ocean"];
+  const middleNames = [
+    "Oakley",
+    "Lennon",
+    "Armani",
+    "Azariah",
+    "Robin",
+    "Smith"
+  ];
+  const lastNames = ["Cypress", "Indiana", "Ridley", "Storm", "Ocean", "Doe"];
 
   // for loop because we want the indices
   for (let i = 0; i < firstNames.length; i += 1) {
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [version] = useState(1);
+  const [fullName, setFullName] = useState("");
+
+  const handleClick = async () => {
+    const response = await fetch("http://localhost:3000/firstName");
+    const json = await response.json();
+    let firstNameID = json[0];
+    let firstName = json[1];
+
+    const midName = await fetch("http://localhost:3000/middleName", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        _id: firstNameID
+      })
+    });
+
+    const secResponse = await midName.json();
+
+    const lastName = await fetch("http://localhost:3000/lastName", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        middleName: secResponse
+      })
+    });
+
+    const lastResponse = await lastName.json();
+    await setFullName(`${firstName} ${secResponse} ${lastResponse}`);
+  };
 
   return (
     <div>
-      <h1>{`gil's boilerplate ${version}`}</h1>
+      <h1>{`Let me name your baby!`}</h1>
+      <button className={"fetchButton"} onClick={() => handleClick()}>
+        Get Name
+      </button>
+      <h3 className={"fullName"}>{fullName}</h3>
     </div>
   );
 };
