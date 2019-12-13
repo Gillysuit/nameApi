7f8fc199aa6f8103ce55789861bac1d1755bbc7a
gillysuit <contemporarygil@gmail.com>
2019-12-12 16:56:52 -0800

    Add working local mongo db with mongoose

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
 const PORT = 3000;
 
+// accept all origins, headers, methods
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
 app.use("/dist", express.static(path.join(__dirname, "../dist")));
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/people", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 8000
+  })
+  .catch(err => console.log("app starting error", err));
+
+mongoose.connection.once("open", () => {
+  console.log(`connection has been made to mongods`);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
-});
-
-app.get("/api", (req, res) => {
-  res.json("api response");
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
